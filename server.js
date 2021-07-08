5be7808bdfc5ab31f0eaf15c470fa99482645ff7
AchrefFast <justnevergiveup001@gmail.com>
2021-07-08 17:13:07 +0100

    Add Comments

--- server.js
+++ server.js
@@ -1,39 +1,47 @@
 var express = require('express');
+// Muter is a nodejs middleware for handling multipart/form-data which is primary used for uploading files.
+// For more information go to :http://expressjs.com/en/resources/middleware/multer.html 
 var multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 var app = express();
-
+// This is an object that controls the way of how we sotre our files.(destination, and filnames)
 var store = multer.diskStorage({
   destination: 'uploades/',
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   }
 });
+// Here we're creating our multer object and we used the "storage" options to choose the way of saving the file.
 var upload = multer({
   storage: store // We also can use dest: "folder" to decide the destination of our storage folder
 });
 
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors()); // For Testing purposes with FreecodeCamp.
+app.use('/public', express.static(process.cwd() + '/public'));// load the static files from public folder
 
+// When visiting the base url send the index.html file from views folder.
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// The POST request when the user upload or submit a file.
 app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+
+  // If the user didn't choose a file
   if (!req.file) {
     throw new Error('You need to choose a file to upload');
   }
   else {
     console.log(req.file);
-    const { originalname, mimetype, size } = req.file;
+    const { originalname, mimetype, size } = req.file; // Using Destructuring assignment to get specific values from the req.file object.
 
     res.json({ "name": originalname, "type": mimetype, "size": size });
   }
 })
 
+// If the user send a wrong request that doesn't match one of the above ones.
 app.all('*', function (req, res) {
   throw new Error('Bad Request');
 })
